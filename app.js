a81e120e921db4adcc78126ab94994ef7b9f86c0
Gabriela Muradás <gmuradas2019@gmail.com>
2025-01-26 22:43:27 +0000

    Projeto final

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+let resultado = document.getElementById("resultado");
+
+function adicionarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    let listaAmigosHTML = document.getElementById("listaAmigos");
+    
+    if (amigo.value = ""){
+        document.getElementById("listaAmigos").innerHTML = "Adicione um nome válido"
+    } else if (listaAmigos.includes(amigo)) {
+        alert("Este nome já foi adicionado à lista");
+    } else if (amigo != "") {
+        listaAmigos.push(amigo);
+        let itemListaAmigos = document.createElement("li");
+        itemListaAmigos.innerHTML = amigo
+        listaAmigosHTML.append(itemListaAmigos);
+    }
+    document.getElementById("amigo").value = "";
+
+}
+
+function sortearAmigo() {
+    if (amigo == "") {
+        resultado.innerHTML = "Adicione um nome, por favor.";
+    } else if (listaAmigos.length == 0) {
+        resultado.innerHTML = "Sem nomes para sortear.";
+    } else if (listaAmigos.length == 1) {
+        resultado.innerHTML = "Adicione mais de um nome para realizar o sorteio.";
+    } else {
+        let amigoAleatorio = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
+        resultado.innerHTML = `Seu amigo secreto é: ${amigoAleatorio}`;
+    }
+}
